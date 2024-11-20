43b97164c9d4229b41dbb22bf6822cb86e14de9d
Skyzimos <95323257+Skyzimos@users.noreply.github.com>
2024-11-20 12:04:30 -0600

    Update index.js

--- api/index.js
+++ api/index.js
@@ -1,1 +1,10 @@
 
+const Express = require('express');
+const App = Express();
+
+App.get('/post/:userid/:addxp', (request, response) => {
+  console.log('hello')
+});
+
+App.get('/', (request, response) => response.send(''));
+App.listen(3000, () => console.log('Database listening at http://localhost:3000'));
